c540caf8ce738514473f86f226b257dfb36490bf
John Johansen <john.johansen@canonical.com>
2020-06-07 04:10:33 -0700

    UPSTREAM: apparmor: Fix memory leak of profile proxy
    When the proxy isn't replaced and the profile is removed, the proxy
    is being leaked resulting in a kmemleak check message.
    
    Make sure to cleanup the profile's embedded label which will result
    on the proxy being properly freed.
    
    Fixes: 637f688dc3dc ("apparmor: switch from profiles to using labels on contexts")
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    (cherry picked from commit 7a8deda17950f425f9560172aa80fbb1fb27ebc7)
    Signed-off-by: Josh Law <objecting@objecting.org>

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -313,10 +313,8 @@ out:
 }
 
 
-static void label_destroy(struct aa_label *label)
+void aa_label_destroy(struct aa_label *label)
 {
-	struct aa_label *tmp;
-
 	AA_BUG(!label);
 
 	if (!label_isprofile(label)) {
@@ -332,16 +330,13 @@ static void label_destroy(struct aa_label *label)
 		}
 	}
 
-	if (rcu_dereference_protected(label->proxy->label, true) == label)
-		rcu_assign_pointer(label->proxy->label, NULL);
-
+	if (label->proxy) {
+		if (rcu_dereference_protected(label->proxy->label, true) == label)
+			rcu_assign_pointer(label->proxy->label, NULL);
+		aa_put_proxy(label->proxy);
+	}
 	aa_free_secid(label->secid);
 
-	tmp = rcu_dereference_protected(label->proxy->label, true);
-	if (tmp == label)
-		rcu_assign_pointer(label->proxy->label, NULL);
-
-	aa_put_proxy(label->proxy);
 	label->proxy = (struct aa_proxy *) PROXY_POISON + 1;
 }
 
@@ -350,7 +345,7 @@ void aa_label_free(struct aa_label *label)
 	if (!label)
 		return;
 
-	label_destroy(label);
+	aa_label_destroy(label);
 	kfree(label);
 }
 
